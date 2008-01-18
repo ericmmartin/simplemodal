dda390ee73347ad27258cbbb69409dd912a4cc85
Eric Martin <eric@ericmmartin.com>
2008-01-18 06:07:03 +0000

    added confirm override demo

---
+++ demo/js/confirm.js
@@ -0,0 +1,46 @@
+/*
+ * SimpleModal Confirm Modal Dialog
+ * http://www.ericmmartin.com/projects/simplemodal/
+ * http://code.google.com/p/simplemodal/
+ *
+ * Copyright (c) 2007 Eric Martin - http://ericmmartin.com
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Revision: $Id$
+ *
+ */
+
+$(document).ready(function () {
+	$('#confirmDialog input:eq(0)').click(function (e) {
+		e.preventDefault();
+
+		// example of calling the confirm function
+		// you must use a callback function to perform the "yes" action
+		confirm("Continue to the SimpleModal Project page?", function () {
+			window.location.href = 'http://www.ericmmartin.com/projects/simplemodal/';
+		});
+	});
+});
+
+function confirm(message, callback) {
+	$('#confirm').modal({
+		close:false, 
+		overlayId:'confirmModalOverlay',
+		containerId:'confirmModalContainer', 
+		onShow: function (dialog) {
+			dialog.data.find('.message').append(message);
+
+			// if the user clicks "yes"
+			dialog.data.find('.yes').click(function () {
+				// call the callback
+				if ($.isFunction(callback)) {
+					callback.apply();
+				}
+				// close the dialog
+				$.modal.close();
+			});
+		}
+	});
+}
