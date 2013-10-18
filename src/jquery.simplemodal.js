184a26a4861c80488eeb37e3510563ad5b394987
Eric Martin <eric@ericmmartin.com>
2013-10-18 13:51:56 -0700

    Add internal boxModel property to deal with IE browsers

--- src/jquery.simplemodal.js
+++ src/jquery.simplemodal.js
@@ -84,6 +84,7 @@
 	};
 	browser.ie6 = browser.msie && /msie 6./.test(ua) && typeof window['XMLHttpRequest'] !== 'object';
 	browser.ie7 = browser.msie && /msie 7.0/.test(ua);
+	browser.boxModel = (document.compatMode === "CSS1Compat");
 
 	/*
 	 * Create and display a modal dialog.
@@ -237,8 +238,7 @@
 				return false;
 			}
 
-			// $.support.boxModel is undefined if checked earlier
-			browser.ieQuirks = browser.msie && !$.support.boxModel;
+			browser.ieQuirks = browser.msie && !browser.boxModel;
 
 			// merge defaults and user options
 			s.o = $.extend({}, $.modal.defaults, options);
